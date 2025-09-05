33d599816ea27a53658d4ba01309d43c4d96e77b
Benjamin Dahrooge <bendahrooge@gmail.com>
2025-09-05 14:16:31 -0400

    Serve at GH pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/rhody-signature",
+  assetPrefix: "/rhody-signature/",
+};
+
+module.exports = nextConfig;
